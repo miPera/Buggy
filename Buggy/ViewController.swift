e8fa8190de813f61f205dcecede33d7aaad41444
miPera <migperalta10@gmail.com>
2017-11-24 20:49:46 -0800

    Added print statement to log sender address

--- Buggy/ViewController.swift
+++ Buggy/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     
     @IBAction func buttonTapped(_ sender: UISwitch) {
         print("Called buttonTapped(_:)");
+        print("sender: \(sender)")
         print("Is control on> \(sender.isOn)")
     }
 }
